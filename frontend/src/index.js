6154bbb22bc96fbe492b0bf7ec9612a1648ec7e6
John Hysong <99369137+johnatticus@users.noreply.github.com>
2023-12-28 20:14:12 -0500

    react router setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>
 );
 
