01e179e4c7ba791606faf4a102a16a237347e92f
John Hysong <99369137+johnatticus@users.noreply.github.com>
2023-12-31 16:38:57 -0500

    middlewear, product routes

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import products from "./data/products.js";
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 connectDB();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use('/api/products', productRoutes);
 
 app.get("/api/products", (req, res) => {
   res.json(products);
