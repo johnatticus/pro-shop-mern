0b5cc87511ed92519ece808c403fc467310aba20
John H <johnatticus@johns-mbp.lan>
2024-01-05 09:06:13 -0500

    jwt auth middleware setup

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 
@@ -16,6 +17,7 @@ const app = express();
 // body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // app.get("/api/products", (req, res) => {
 //   res.json(products);
