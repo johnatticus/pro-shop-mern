c8dba38340c9f183abc489cf2fe5e5172adc896f
John Hysong <99369137+johnatticus@users.noreply.github.com>
2024-01-08 21:57:54 -0500

    private route setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -27,7 +28,9 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />;
-      <Route path='/shipping' element={<ShippingScreen />} />;
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingScreen />} />;
+      </Route>
     </Route>
   )
 );
