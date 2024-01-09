11842067aaa572d15d59ce39ad5fa4e53d91c819
John Hysong <99369137+johnatticus@users.noreply.github.com>
2024-01-09 18:09:26 -0500

    order screen and slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,6 +18,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 import PrivateRoute from './components/PrivateRoute';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} />;
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
       </Route>
     </Route>
   )
