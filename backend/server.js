32afb661bf4d8ce7a731a5788f5fa24168661ebb
John Hysong <99369137+johnatticus@users.noreply.github.com>
2024-01-10 21:33:48 -0500

    image upload backend setup

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const port = process.env.PORT || 5000;
 
@@ -28,14 +30,25 @@ app.use(cookieParser());
 //   const product = products.find((p) => p._id === req.params.id);
 //   res.json(product);
 // });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
