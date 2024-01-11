8f3377fe22beacc6e21be1c30b51d3b556376a37
John Hysong <99369137+johnatticus@users.noreply.github.com>
2024-01-11 14:27:49 -0500

    user list screen

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,35 +1,42 @@
-import { apiSlice } from './apiSlice';
-import { USERS_URL } from '../constants';
+import { apiSlice } from "./apiSlice";
+import { USERS_URL } from "../constants";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/login`,
-        method: 'POST',
+        method: "POST",
         body: data,
       }),
     }),
     register: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}`,
-        method: 'POST',
+        method: "POST",
         body: data,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
         url: `${USERS_URL}/logout`,
-        method: 'POST',
+        method: "POST",
+      }),
     }),
-  }),
     profile: builder.mutation({
       query: (data) => ({
         url: `${USERS_URL}/profile`,
-        method: 'PUT',
+        method: "PUT",
         body: data,
-}),
-    })
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ["User"],
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -38,4 +45,5 @@ export const {
   useLogoutMutation,
   useRegisterMutation,
   useProfileMutation,
+  useGetUsersQuery,
 } = usersApiSlice;
